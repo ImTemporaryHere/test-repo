f4a090d2788d6284c9fb876f7147be4c1903eb26
pavlo-pantus-cloudeou <pavlo.pantus@cloudeou.com>
2023-01-05 08:39:52 +0200

    added option to ts config. to include package.json in build

--- index.ts
+++ index.ts
@@ -1,2 +1,2 @@
 export {logActions} from "./module";
-
+export {sourceModuleLogger} from './src/source-folder/inner-source-folder/scrcmodule'
---
+++ src/source-folder/inner-source-folder/scrcmodule.ts
@@ -0,0 +1,3 @@
+export const sourceModuleLogger = (a:string) => {
+  console.log(a)
+}
